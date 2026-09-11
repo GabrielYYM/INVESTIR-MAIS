c3e161bf9c51e51f5f9ee734a904be8efda7e46a
GabrielYYM <gabrielyyminato@gmail.com>
2026-09-11 06:56:37 -0300

    chore: initial project setup

---
+++ src/test/java/com/example/investirMais/InvestirMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investirMais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestirMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
