71c5ab3863527de41746c4aa75040f09752527f1
Lucas Tadaieski <dev.lucastadaieski@gmail.com>
2026-09-13 21:57:11 -0300

    refactor: Renomeando as classes pra deixar de acordo com o projeto

--- src/main/java/com/repositorio/investir_mais/InvestirMaisApplication.java
+++ src/main/java/com/repositorio/investir_mais/InvestirMaisApplication.java
@@ -1,13 +1,13 @@
-package com.repositorio.mvp;
+package com.repositorio.investir_mais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvpApplication {
+public class InvestirMaisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MvpApplication.class, args);
+		SpringApplication.run(InvestirMaisApplication.class, args);
 	}
 
 }
--- src/test/java/com/repositorio/investir_mais/MvpApplicationTests.java
+++ src/test/java/com/repositorio/investir_mais/MvpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.repositorio.mvp;
+package com.repositorio.investir_mais;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
